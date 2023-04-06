5cfd98b8507a7de8bb9a4026682ffb461f0168ba
Alin Nedelcu <anedelcu104@gmail.com>
2023-04-06 16:32:05 -0500

    First Commit

---
+++ employeemanager/src/main/java/com/neverstoplearning/employeemanager/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.neverstoplearning.employeemanager.service;
+
+import com.neverstoplearning.employeemanager.exception.UserNotFoundException;
+import com.neverstoplearning.employeemanager.model.Employee;
+import com.neverstoplearning.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+    @Autowired
+    private final EmployeeRepo employeeRepo;
+
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+}
